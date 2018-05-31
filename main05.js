83861aa4e274b3c7051a5dd6b1c6ff067a1cf423
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-05-31 19:40:58 +0900

    1.検索ボタン押下時に、入力テキストを表示させる。

--- main05.js
+++ main05.js
@@ -1,1 +1,10 @@
 var fruits = ['apple', 'apricot', 'avocado', 'blueberry', 'cherry', 'coconut', 'cranberry', 'dragonfruit', 'durian', 'grape', 'grapefruit', 'guava', 'kiwi fruit', 'lemon', 'lime', 'lychee', 'mango', 'melon', 'watermelon', 'miracle fruit', 'orange', 'bloodorange','clementine','mandarine','tangerine','papaya','passionfruit','peach','pear','persimmon','physalis','plum/prune','pineapple','pomegranate','raspberry','rambutan','star fruit','strawberry'];
+
+$(function() {
+  $("#submit").on("click", function() {
+    var input = $("#keyword").val();
+    $("#result").text(input);
+  });
+
+
+})

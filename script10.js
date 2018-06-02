964dae75421ea7a6cb7cc50fe31b148e52f0f1a3
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-02 22:16:17 +0900

    より高度なイベントを学習

--- script10.js
+++ script10.js
@@ -1,1 +1,9 @@
-
+$(function() {
+  var card = $(".card");
+  card.on("click", function() {
+  // 『this』によるイベント発生元の取得
+  // console.log(this);
+    var img = $(this).find("img");
+    img.css("opacity", "0.5");
+  });
+});

6adde7e2680121279d3892d402ae7583842fb5b6
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-03 17:28:01 +0900

    $.ajax()を使ってAPIを叩く（3）
    jsでDOMを書き換える。

--- script.js
+++ script.js
@@ -9,13 +9,22 @@ $(function() {
     var cityName = $(this).find("#weather-form-city").prop("value");
     // ドキュメントを参照に、渡し方をきちんと指定
     // 文字列の結合は『+』が必要
-    $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID="+ API_KEY + "&q=" + cityName);
+    $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID="+ API_KEY + "&q=" + cityName)
     // $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID=<API Key>&q=<都市名>")
-      .done(function() {
-        // 通信成功時の処理
-      });
-      .fail(function() {
-        // 通信失敗時の処理
+      // 通信成功時の処理
+      // 処理後に『;』があるとエラーとなった
+      .done(function(data) {
+        $("#form-error").css("display", "none");
+        $("#result-city-name").text(data.name);
+        $("#result-temp").text(Math.round(data.main.temp - 273));
+        $("#result-weather").text(data.weather[0].main);
+        $("#result-datetime").text(Date(data.dt));
+        $("#form-result").css("display", "block");
+      })
+      // 通信失敗時の処理
+      .fail(function(data) {
+        $("#form-spinner").css("display", "none");
+        alert("Something wrong occurred.");
       });
     e.preventDefault();
   });

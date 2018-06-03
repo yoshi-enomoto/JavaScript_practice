d69013f1c1ada6529cba3cce38cbfa1f279a1528
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-03 16:39:29 +0900

    イベントハンドラの設定

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // 取得したAPI Keyを文字列として定義
 const API_KEY = "853595dafb5bd7741172c7c11b604980"
 
+// イベントハンドラを設定
+// inputのtextではなく、form全体をイベントに設定
+$(function() {
+  $("#weather-form").on("submit", function(e) {
+    e.preventDefault();
+  });
+});

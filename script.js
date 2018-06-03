2f6719352fc867fc482f880564a98cf2aae59513
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-03 17:03:06 +0900

    $.ajax()を使ってAPIを叩く（1-2）
    基本記述方法に沿って、正式な書き方に書き換える。

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ $(function() {
   $("#weather-form").on("submit", function(e) {
     // ユーザーの入力値の取得
     var cityName = $(this).find("#weather-form-city").prop("value");
-    $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID=<API Key>&q=<都市名>")
+    // ドキュメントを参照に、渡し方をきちんと指定
+    // 文字列の結合は『+』が必要
+    $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID="+ API_KEY + "&q=" + cityName);
+    // $.ajax("http://api.openweathermap.org/data/2.5/weather?APPID=<API Key>&q=<都市名>")
     e.preventDefault();
   });
 });
